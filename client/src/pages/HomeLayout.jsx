050c64dd4450ae182f9465905dad703551a34c93
Asham9 <87985316+Asham9@users.noreply.github.com>
2025-09-24 15:40:12 +0545

    Design immersive home page with refined layout

--- client/src/pages/HomeLayout.jsx
+++ client/src/pages/HomeLayout.jsx
@@ -1,14 +1,16 @@
-
-import { Outlet } from 'react-router-dom'
-import {Navbar} from '../components'
+import { Outlet } from 'react-router-dom';
+import { Navbar, Footer } from '../components';
 
 const HomeLayout = () => {
   return (
-    <div>
-      <Navbar/>
-      <Outlet/>
+    <div className="flex min-h-screen flex-col bg-slate-50">
+      <Navbar />
+      <main className="flex-1 pt-24">
+        <Outlet />
+      </main>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default HomeLayout
+export default HomeLayout;
